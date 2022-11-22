571ce89c9fca40c956df2e9d6aab318503b62309
Pramod kumar Nagaraj <pramod-kumar.nagaraj@epita.fr>
2022-11-22 21:32:58 +0100

    cache error

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
-st.set_page_config(page_title="Detecttion", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Detection", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-@__cached__
+@cache
 def load_image(image):
     image = Image.open(image)
     return image
